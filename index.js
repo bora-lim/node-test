4f9418bd604123102b07aaf7844e0ff01b9269aa
bora-lim <purplelim00@gmail.com>
2022-02-14 22:18:31 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+
+/* 해당 형식의 데이터를 파싱해줌 */
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended : true}));
+// application/json
+app.use(bodyParser.json());
+
+
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://vivi:vivi1234@practice.7rb2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 아ㄴ녕!!')
+  res.send('Hello World! 안녕~~')
+})
+
+app.post('/register', (req, res) => {
+
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success : false, err })
+    return res.status(200).json({
+      success : true
+    })
+  })
 })
 
 app.listen(port, () => {
